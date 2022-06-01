7f8b3534890fb7de86da718e182261a35163db52
DiegoApro <diegoaprosoff@gmail.com>
2022-06-01 19:04:09 -0300

    actualizacion ej2

--- intro/src/main/CarreraDeLaSelva.java
+++ intro/src/main/CarreraDeLaSelva.java
@@ -8,55 +8,68 @@ public class CarreraDeLaSelva {  public static void main(String[] args) {
     String[] circuitoMedio = {};
     String[] circuitoGrande = {};
 
+    String[] [] participantes = {};  // {{DNI}, {nombre}, {apellido}, {edad}, {celular}, {número de participante}}
+
     int precioCircuitoChicoMenor = 1300;
     int precioCircuitoChicoMayor = 1500;
     int precioCircuitoMedianoMenor = 2000;
     int precioCircuitoMedianoMayor = 2300;
     int precioCircuitoGrande = 2800;
 
-    String[] participantes = {};
-
     // int edad = 0;
     int numeroParticipante = 0;
     int montoAbonar = 0;
 
-    Scanner lectura = new Scanner (System.in);
-    System.out.print("ingrese su DNI: ");
-    int dni = lectura.nextInt();
-    System.out.print("ingrese su nombre: ");
-    String nombre = lectura.next();
-    System.out.print("ingrese su apellido: ");
-    String apellido = lectura.next();
-    System.out.print("ingrese su edad: ");
-    int celular = lectura.nextInt();
-    System.out.print("ingrese su numero de emergencia: ");
-    int numeroEmergencia = lectura.nextInt();
-    System.out.print("ingrese su grupo sanguiño: ");
-    String sangre = lectura.next();
-    int edad = lectura.nextInt();
-    System.out.print("ingrese su celular: ");
+    Scanner entrada = new Scanner (System.in);
 
-    System.out.print("si quiere competir en el Circuito Chico, coloque 1, en caso que sea en el Circuito Mediano coloque 2, y sino 3 ");
-    String tipoCompe = lectura.next();
+    System.out.print("ingrese su DNI: ");
+    int dni = entrada.nextInt();
+
+    System.out.print("ingrese su nombre: ");
+    String nombre = entrada.next();
+
+    System.out.print("ingrese su apellido: ");
+    String apellido = entrada.next();
+
+    System.out.print("ingrese su edad: ");
+    int edad = entrada.nextInt();
+
+
+    System.out.print("ingrese su numero de emergencia: ");
+    int numeroEmergencia = entrada.nextInt();
+
+    System.out.print("ingrese su grupo sanguiño: ");
+    String sangre = entrada.next();
+
+
+    System.out.print("ingrese su celular: ");
+    int celular = entrada.nextInt();
+
+    System.out.print("si quiere competir en el Circuito Chico, coloque 1, en caso que sea en el Circuito Mediano coloque 2, y sino 3: ");
+    int tipoCompe = entrada.nextInt();
+
 
     if (tipoCompe == 1){
-        circuitoChico.add(nombre);
         if (edad >= 18){
             System.out.print("Ústed debera abonar " + precioCircuitoChicoMayor);
-        }
+        } else {
         System.out.print("Ústed debera abonar " + precioCircuitoChicoMenor);
+        }
     }
     if (tipoCompe == 2){
-        circuitoMedio.add(nombre);
         if (edad >= 18){
             System.out.print("Ústed debera abonar " + precioCircuitoMedianoMayor);
+        } else {
+            System.out.print("Ústed debera abonar " + precioCircuitoMedianoMenor);
         }
-        System.out.print("Ústed debera abonar " + precioCircuitoMedianoMenor);
     }
-    if (tipoCompe == 3 && edad >= 18){
-        circuitoGrande.add(nombre);
-        System.out.print("Ústed debera abonar " + precioCircuitoGrande);
+    if (tipoCompe == 3){
+        if (edad < 18){
+            System.out.print("Ústed no puede partipar en el circuito grande por ser menor de edad");
+        }else {
+            System.out.print("Ústed debera abonar " + precioCircuitoGrande);
+        }
+    }
 
-    }
-};
+    };
 }
